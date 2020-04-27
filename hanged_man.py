3faf63c8e880491b60465cdea9ef1071f452913f
Maxime48 <37154360+Maxime48@users.noreply.github.com>
2020-04-27 10:59:34 +0200

    Update hanged_man.py
    Updated to show the word at the end of the game

--- hanged_man.py
+++ hanged_man.py
@@ -1,4 +1,19 @@
+from random import *
 from tkinter import *
+table = open("liste_mots.txt", "rt").read().split()
+
+def nombre_mots():
+    return len(table)
+
+def mot_choisi():
+    return table[randint(1, nombre_mots())]
+
+class game:
+    label = ""
+    current = ""
+    def __init__(self):
+        main_canvas.config()
+        word.replace = word.star()
 
 class main_canvas:
     title = "Le pendu"
@@ -7,7 +22,7 @@ class main_canvas:
     def config():
         PENDU=Tk()
         PENDU.title(main_canvas.title)
-        PENDU.configure(bg="grey",width=300,heigh=450)
+        PENDU.configure(bg="grey",width=600,heigh=450)
         dessin=Canvas(PENDU,bg="white",width=200,height=350)
         dessin.place(x=50,y=50)
         def ne_xt():
@@ -38,9 +53,61 @@ class main_canvas:
                 dessin.update()
                 main_canvas.int_text="PERDU"
                 Label(PENDU,text=main_canvas.int_text).place(x=130,y=400)
+                print(word().mot)
             main_canvas.internal += 1
-        Button(PENDU,text="SUIVANT",command=ne_xt).place(x=125,y=425)
+        if "-" in word().mot:
+            stat = "contient"
+        else:
+            stat = "ne contient pas de"
+            forma_t = "Ce mot "+stat+" caractère spécial"
+            Label(PENDU,text=forma_t).place(x=300,y=400)
+        def valid():
+            if word.replace.count("*")>0:
+               if word.demande(Lettre.get()) == False:
+                    ne_xt()
+               else:
+                    game.label = game.current
+                    label.config(text = game.label)
+            else:
+                main_canvas.int_text="Gagné"
+                Label(PENDU,text=main_canvas.int_text).place(x=130,y=400)
+        label = Label(PENDU,text=game.label)
+        label.place(x=400,y=100)
+        Lettre=Entry(PENDU) #au secour je suis fatigué
+        Lettre.place(x=0,y=425)
+        Button(PENDU,text="Valider",command=valid).place(x=200,y=425)
         PENDU.mainloop
-        
-main_canvas.config()  
-
+  
+class word:
+    choice = mot_choisi()
+    fail = 0
+    f_max = 6
+    replace = ""
+    def __init__(self):
+        self.mot = word.choice
+        self.len = len(self.mot)
+    def star():
+        mot = "*"
+        for a in range(word().len-1):
+            mot += "*"
+        return mot
+    def demande(letter):
+        if letter == "":
+            print("Entrez une lettre !")
+        elif letter.upper() in word().mot:
+            other = list(word().mot)
+            word.replace = list(word.replace)
+            for i in range(len(word.replace)):
+                if other[i] == letter.upper():
+                    word.replace[i] = letter
+            word.replace = "".join(word.replace)
+            game.current = word.replace
+            return True
+        else:
+              return False
+    #def result():
+        #if word.fail == 6:
+            #print("Vous avez perdu! Le mot était: ",word().mot)
+        #else:
+            #print("Vous avez gagné !")
+game()
